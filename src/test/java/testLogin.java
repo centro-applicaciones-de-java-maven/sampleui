7a9c92d85f6759a8e86b1270d02c2ac1f63e10b1
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2026-06-20 14:59:06 +0800

    20260620

--- src/test/java/testLogin.java
+++ src/test/java/testLogin.java
@@ -1,7 +1,9 @@
 
 import javafx.application.Application;
+import org.guanzon.appdriver.base.GRider;
 import org.guanzon.appdriver.base.GRiderCAS;
 import ph.com.guanzongroup.login.Login;
+import ph.com.guanzongroup.login.LoginOLD;
 
 public class testLogin {
     public static void main(String[] args) throws Exception {
@@ -12,7 +14,17 @@ public class testLogin {
         
         GRiderCAS gRider = login.getGRider();
         
-        System.out.println(gRider.getProductID());
-        System.out.println(gRider.getUserID());
+        System.out.println(login.getProductId());
+        System.out.println(login.getUserId());
+        
+//        LoginOLD login = new LoginOLD();
+//        login.setProductId("gRider");
+//        
+//        Application.launch(login.getClass());
+//        
+//        GRider gRider = login.getGRider();
+//        
+//        System.out.println(login.getProductId());
+//        System.out.println(login.getUserId());
     }
 }
